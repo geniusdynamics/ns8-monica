bdc37c128b911b649267435de0a775c32b1087b5
Kemboi Elvis <kemboielvis22@gmail.com>
2025-12-12 02:19:44 +0300

    feat: moved back to 10

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
 	--label="org.nethserver.authorizations=traefik@node:routeadm" \
 	--label="org.nethserver.tcp-ports-demand=1" \
 	--label="org.nethserver.rootfull=0" \
-	--label="org.nethserver.images=docker.io/library/redis:7.2.3-bookworm  docker.io/getmeili/meilisearch:latest docker.io/monica:${APP_VERSION} docker.io/mariadb:11" \
+	--label="org.nethserver.images=docker.io/library/redis:7.2.3-bookworm  docker.io/getmeili/meilisearch:latest docker.io/monica:${APP_VERSION} docker.io/mariadb:10.11.5" \
 	"${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
